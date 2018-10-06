2eb8d7f043fc968d79173d710cc7bde45f7ca1ff
David Chia <david.mhchia@gmail.com>
2018-10-06 14:40:04 +0100

    Add pagination and input validation to getNotes endpoint

--- server/views.py
+++ server/views.py
@@ -1,15 +1,28 @@
-from flask_restplus import Api, Resource
+from flask_restplus import Api, Resource, Model, fields
 
 from base import db, app
 from models import Note
 import utils
 
 
-api = Api(app, title="Vault API", prefix='/v1')
+api = Api(app, title="Vault API", prefix='/v1', validate=True)
+
+ns = api.default_namespace
+
+get_notes_fields = {
+    'page': fields.Integer(required=True),
+    'pagesize': fields.Integer(required=True),
+}
+get_notes_model = api.model('get_notes', get_notes_fields)
 
 
 @api.route("/notes")
 class Notes(Resource):
-    def get(self):
-        notes = db.session.query(Note).all()
+
+    @ns.expect(get_notes_model)
+    def post(self):
+        page = api.payload['page']
+        page_size = api.payload['pagesize']
+        offset = page * page_size
+        notes = db.session.query(Note).offset(offset).limit(page_size).all()
         return utils.rows_as_json(notes)
